8b0205cee4af48e03f9eb33aba3d901ef095cb4b
Joudat <joudat01@gmail.com>
2024-08-03 16:23:00 -0400

    Updated formatting

--- prototypes/textract/index.js
+++ prototypes/textract/index.js
@@ -27,33 +27,16 @@ const params = {
   }
 
 const displayBlockInfo = async (response) => {
+  let textBlock = [];
+  let textString = "";
     try {
         response.Blocks.forEach(block => {
-            console.log(`ID: ${block.Id}`)
-            console.log(`Block Type: ${block.BlockType}`)
             if ("Text" in block && block.Text !== undefined){
-                console.log(`Text: ${block.Text}`)
+                textBlock.push(block.Text)
             }
-            else{}
-            if ("Confidence" in block && block.Confidence !== undefined){
-                console.log(`Confidence: ${block.Confidence}`)
-            }
-            else{}
-            if (block.BlockType == 'CELL'){
-                console.log("Cell info:")
-                console.log(`   Column Index - ${block.ColumnIndex}`)
-                console.log(`   Row - ${block.RowIndex}`)
-                console.log(`   Column Span - ${block.ColumnSpan}`)
-                console.log(`   Row Span - ${block.RowSpan}`)
-            }
-            if ("Relationships" in block && block.Relationships !== undefined){
-                console.log(block.Relationships)
-                console.log("Geometry:")
-                console.log(`   Bounding Box - ${JSON.stringify(block.Geometry.BoundingBox)}`)
-                console.log(`   Polygon - ${JSON.stringify(block.Geometry.Polygon)}`)
-            }
-            console.log("-----")
         });
+        textString = textBlock.join(" ");
+        console.log(textString);
       } catch (err) {
         console.log("Error", err);
       }
