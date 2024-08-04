d332048278cb546eabc3ad1d4fde9b800acbfa1f
David Pietrocola <pietrocoladavid@gmail.com>
2024-08-03 21:26:46 -0400

    FIXME: hardcode token

--- webapp/src/app/dashboard/page.tsx
+++ webapp/src/app/dashboard/page.tsx
@@ -8,13 +8,15 @@ import { getAccessToken, getCurrentlyPlayingFetcher } from "@/lib/spotify";
 
 export default function Home() {
   const handleClick = async () => {
-   const token = await getAccessToken();
-    const data = await getCurrentlyPlayingFetcher("BQA8FKpx3yVO2GE0kIvWUSMVjI6tYalUaB-O1S35Us-bk9nvCWXQBLVFthXONnXkVJjkorxU0LYKSUSa9f0zRl63k42l-R2boqLyQyaR64AbRh54IgUUzu6vNIwiJrlKubyxshqm7kIg_FBKcZOO6CmFLiE3GXcuN_UMunb_4JHXxovm2oNc7mmj6Khezo_CrYiM");
+    const token = await getAccessToken();
+    console.log("TOKEEEEEN")
+    console.log(token)
+    const data = await getCurrentlyPlayingFetcher(token);
     console.log("ciao")
     console.log(data);
   };
 
-  return (    
+  return (
     <>
       <Topbar />
       <section className="w-full py-20 gap-10 flex flex-col justify-center">
@@ -24,7 +26,9 @@ export default function Home() {
         <div className="w-full flex justify-center">
           <BookOpen size={256} />
         </div>
-        <Button className="w-full md:w-1/3" onClick={handleClick}>Get Started</Button>
+        <div className="w-full justify-center flex">
+          <Button className="w- md:w-1/3" onClick={handleClick}>Get Started</Button>
+        </div>
       </section>
     </>
   );
